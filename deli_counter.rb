1c555c3cd469de4dc7f905cab2315a66c61f64b6
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-27 21:02:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   elsif
     first = "The line is currently:"
-    katz_deli.each {|name| puts "#{katz_deli.index(name) + 1}. #{name}"
+    katz_deli.each {|name| puts "#{katz_deli.index(name) + 1}. #{name}"}
   end
   end
 end
