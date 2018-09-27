d35e0c5858f22a46c538eaa1cb19167dccd6927f
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-27 21:12:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
-  elsif
+  else
     numsNames = []
     first = "The line is currently:"
     katz_deli.each {|name| numsNames << "#{katz_deli.index(name) + 1}. #{name}"}
