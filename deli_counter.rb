93a87307cf0ba4405bbd13b82b3cd290926ea183
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-27 20:55:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,6 @@
 katz_deli = []
-def line(katz_deli, name)
+def line(katz_deli)
   if katz_deli.size == 0 
     "The line is currently empty."
-  elsif
-    katz_deli << name
   end
-  katz_deli
 end
